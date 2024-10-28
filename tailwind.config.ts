5bf4946af267210356ee41f99be9d030a1ebb0e6
Solaris470 <77920188+Solaris470@users.noreply.github.com>
2024-10-29 04:11:48 +0700

    Apex Chart JS :)

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import flowbite from "flowbite-react/tailwind";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbite.plugin(),
+    require("flowbite/plugin")({
+      charts: true,
+    }),
+  ],
+};
+export default config;
